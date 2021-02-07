026067a6924092a8162078b81118a63f19b2d3e8
winexe0 <47309918+winexe0@users.noreply.github.com>
2021-02-06 17:23:27 -0800

    Add autoconf and automake check

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 echo Please wait, as this may take a while
-sudo apt-get install gawk gcc g++ make -y  --allow-unauthenticated
+sudo apt-get install gawk gcc g++ make autoconf automake -y  --allow-unauthenticated
 aclocal 
 autoconf
 automake --add-missing
